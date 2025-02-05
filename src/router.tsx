a62a4e7f52c9401fb81d99a7e6503a5e6fd5e96a
euni00 <amy000809@gmail.com>
2025-02-05 22:25:56 +0900

    ✨ Feat: 메인페이지 추가

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './home.tsx';
+import MainPageContainer from './pages/MainPage';
 
 const AppRouter = () => {
-    return(
+    return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path="/" element={<Home/>}>
+                    <Route index element={<MainPageContainer/>}/>
+                </Route>
             </Routes>
         </BrowserRouter>
     );
-}
+};
 
 export default AppRouter;
