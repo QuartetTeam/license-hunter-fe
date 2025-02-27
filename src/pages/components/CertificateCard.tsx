fed4530de6476a32a9fc5d7e6e6ce728d6f57ed2
euni00 <amy000809@gmail.com>
2025-02-28 01:03:51 +0900

    feat: 메인페이지 & 자격증 상세 카드 컴포넌트 퍼블리싱 완료

--- src/pages/components/CertificateCard.tsx
+++ src/pages/components/CertificateCard.tsx
@@ -35,22 +35,25 @@ const CertificateCard: FC<CertificateCardProps> = ({ mockData = [], trashIconSel
                         <div className="recommend-certificate-image"></div>
                         <div className="recommend-certificate-text">
                             <div className="recommend-certificate-name">{item.certificateName}</div>
-                            <div className="recommend-certificate-receipt">접수일
-                                | {item.receiptDate}</div>
-                            <div className="recommend-certificate-test">시험일 | {item.testDate}</div>
-                            <div className="recommend-certificate-calender">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     strokeWidth={1.5}
-                                     stroke="currentColor" className="recommend-certificate-calender-icon">
-                                    <path strokeLinecap="round" strokeLinejoin="round"
-                                          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"/>
-                                </svg>
-                                <div className="recommend-certificate-calender-count">
-                                    {item.calenderCount}
-                                </div>
+                            <div className="recommend-certificate-date">
+                                <div className="recommend-certificate-date__receipt">접수일
+                                    | {item.receiptDate}</div>
+                                <div className="recommend-certificate-date__test">시험일 | {item.testDate}</div>
+                            </div>
+                        </div>
+                        <div className="recommend-certificate-calender">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 strokeWidth={1.5}
+                                 stroke="currentColor" className="recommend-certificate-calender__icon">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"/>
+                            </svg>
+                            <div className="recommend-certificate-calender__count">
+                                {item.calenderCount}
                             </div>
                         </div>
                     </div>
+
                 </div>
             ))}
 
