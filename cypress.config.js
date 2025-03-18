570f6279e34fc65f73d2fbdf008d5d009763e24e
devlee <amy000809@gmail.com>
2025-03-18 22:52:20 +0900

    [QUAR-37] 📝 Rename: 테스트 코드 폴더 이름 e2e -> unit으로 변경

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from "vite";
 
 export default defineConfig({
     projectId: "53s32p",
-    e2e: {
+    unit: {
         baseUrl: "http://localhost:5173/",
         viewportWidth: 1440,
         viewportHeight: 720,
