8d17e0d304ebc721a7c317367cbce852d36d0625
devlee <amy000809@gmail.com>
2025-04-16 03:12:40 +0900

    [QUAR-38] ✨ Feat: FE proxy 수정 (04/16 지희님, 현제님)

--- vite.config.ts
+++ vite.config.ts
@@ -18,9 +18,9 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://34.64.253.116:8080/api/v1',
-        target: 'http://quartet-load-balancer-602448148.ap-northeast-2.elb.amazonaws.com/api/v1',
+        target: 'https://api.sudomenda.site/api/v1',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/path/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
         ws: true,
       },
