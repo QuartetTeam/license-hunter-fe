9042fb35363731c638af825d723c1be13cb444c5
devlee <amy000809@gmail.com>
2025-04-19 23:55:35 +0900

    [QUAR-38] ✨ Feat: FieldButton 컴포넌트 업데이트에 따른 storybook 업데이트

--- src/stories/FieldButton/FieldButton.tsx
+++ src/stories/FieldButton/FieldButton.tsx
@@ -1,20 +1,31 @@
-import IconList from '@component/icons/fieldIconsList.ts';
-import { Field } from '@component/types/Field.ts';
+import { useCertService } from '@feature/Certification/useCertService.ts';
+import { fieldIconsMap } from '@component/icons/fieldIconsList.ts';
+import { fieldNameToEnumMap } from '@component/types/fieldNameToEnumMap.ts';
 import '@component/favoriteFieldButton.scss';
-
-interface IFavoriteFieldButtonProps {
-  /** Insert Field Name you want */
-  fieldName: Field;
-}
 
 /** FieldButton Component for Main Page, MyPage, CheckFavoriteField Modal.
  * Background-color, border-radius will be change where we use this button.*/
-const FieldButton = ({ fieldName }: IFavoriteFieldButtonProps) => {
-  const FieldIcon = IconList[fieldName];
+const FieldButton = ({
+  fieldName,
+  id,
+  name,
+}: {
+  fieldName: string;
+  id?: string;
+  name?: string;
+}) => {
+  const { moveToCertById } = useCertService();
+  const fieldEnum = fieldNameToEnumMap[fieldName];
+  const FieldIcon = fieldIconsMap[fieldEnum];
 
   return (
-    <button className="favorite-field-button">
-      <FieldIcon />
+    <button
+      className="favorite-field-button"
+      onClick={() => {
+        if (id && name) moveToCertById(id, name, undefined);
+      }}
+    >
+      {FieldIcon && <FieldIcon />}
       {fieldName}
     </button>
   );
