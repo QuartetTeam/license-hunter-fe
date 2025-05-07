bf0a1681172c316ddf815b0428397c483e80baa3
devlee <amy000809@gmail.com>
2025-05-07 22:53:01 +0900

    [QUAR-68] ✨ Feat: 로그인 상태에 따라 로그인, 로그아웃, 마이페이지, 내 캘린더 버튼 분기처리

--- src/common/Layouts/Header.tsx
+++ src/common/Layouts/Header.tsx
@@ -1,8 +1,17 @@
 import { useMoveToPage } from '@hook/page.ts';
+import useAuthService from '@feature/Auth/useAuthService.ts';
+import authStore from '@store/auth/authStore.ts';
 import './style/Header.scss';
 
 const Header = () => {
+  const { accessToken } = authStore();
+  const { logoutService } = useAuthService();
   const moveToPage = useMoveToPage();
+
+  const onClickLogout = async () => {
+    await logoutService();
+    moveToPage('/');
+  };
 
   return (
     <div className="header">
@@ -11,29 +20,37 @@ const Header = () => {
           자격저격
         </div>
         <div className="header-button-group">
-          <button className="my-calendar-button" onClick={() => moveToPage('/myCalendar')}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="my-calendar-button-icon"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
-              />
-            </svg>
-            내 캘린더
-          </button>
-          <button className="login-button" onClick={() => moveToPage('/login')}>
-            로그인
-          </button>
-          <button className="myPage-button" onClick={() => moveToPage('/myPage')}>
-            마이페이지
-          </button>
+          {!accessToken ? (
+            <button className="login-button" onClick={() => moveToPage('/login')}>
+              로그인
+            </button>
+          ) : (
+            <>
+              <button className="my-calendar-button" onClick={() => moveToPage('/myCalendar')}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="my-calendar-button-icon"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                  />
+                </svg>
+                내 캘린더
+              </button>
+              <button className="myPage-button" onClick={() => moveToPage('/myPage')}>
+                마이페이지
+              </button>
+              <button className="logout-button" onClick={onClickLogout}>
+                로그아웃
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
