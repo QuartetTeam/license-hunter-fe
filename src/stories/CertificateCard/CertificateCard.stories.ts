46d510aacf5a05df461ca2fa07acbc0cd7f35378
devlee <amy000809@gmail.com>
2025-06-30 23:56:13 +0900

    [QUAR-75] 📋 Docs: CertificateCard 컴포넌트 수정에 따른 Storybook 업데이트

--- src/stories/CertificateCard/CertificateCard.stories.ts
+++ src/stories/CertificateCard/CertificateCard.stories.ts
@@ -27,7 +27,25 @@ export const Certificate: Story = {
         calendarSubscription: 0,
       },
     ],
-    trashIconSelected: false,
+    isCheckBox: false,
+    // requiredCalendar: true,
+  },
+};
+
+export const CertificateWithCheckBox: Story = {
+  args: {
+    data: [
+      {
+        id: 575,
+        mainCategory: '경매·중개',
+        subCategory: '유통·물류',
+        name: '경매사(수산)',
+        applicationDate: '2025-05-19T09:00:00Z',
+        examDate: '2025-06-21T09:00:00Z',
+        calendarSubscription: 0,
+      },
+    ],
+    isCheckBox: true,
     // requiredCalendar: true,
   },
 };
--- src/stories/CertificateCard/CertificateCard.tsx
+++ src/stories/CertificateCard/CertificateCard.tsx
@@ -1,20 +1,23 @@
 import CheckboxChecked from '@icon/icon-checkbox-check.svg?react';
 import CheckboxEmpty from '@icon/icon-checkbox-empty.svg?react';
+import { useCertService } from '@feature/Certification/useCertService.ts';
 import mailingStore from '@store/mailing/mailingStore';
 import { ICertData } from '@type/cert.ts';
 import { IMailingContent } from '@type/mailing.ts';
-import './certificateCard.scss';
+import QNetImage from '@image/QNet.png';
+import './style/certificateCard.scss';
 
 /** Certificate Card Component for Recommend Certificate and Certificate Search */
 const CertificateCard = ({
   data,
-  trashIconSelected = false,
+  isCheckBox,
   // requiredCalendar = false
 }: {
   data?: ICertData[] | IMailingContent[];
-  trashIconSelected?: boolean;
+  isCheckBox: boolean;
 }) => {
   const { checkArr, setCheckArr } = mailingStore();
+  const { formatDate, moveToCertDetailById } = useCertService();
 
   const handleCheckArr = (mailingsId: number) => {
     if (checkArr.includes(mailingsId)) {
@@ -24,63 +27,68 @@ const CertificateCard = ({
     }
   };
 
-  const formatDate = (date: string | string[]) => {
-    if (Array.isArray(date)) {
-      const dateArray = date.map((item) => item.split('T')[0]);
-      return dateArray.join(' ~ ');
-    } else {
-      if (date === '') {
-        return '';
-      }
-      return date.split('T')[0];
-    }
+  const isMailingContent = (item: IMailingContent | ICertData): item is IMailingContent => {
+    return 'mailingId' in item;
   };
 
   return (
     <>
-      {data?.map((item, index) => (
-        <div key={index} className="certificate-info">
-          {trashIconSelected && (
-            <div className="certificate-info-checkbox">
-              {checkArr.includes(item.id) ? (
-                <CheckboxChecked onClick={() => handleCheckArr(item.id)} />
-              ) : (
-                <CheckboxEmpty onClick={() => handleCheckArr(item.id)} />
-              )}
-            </div>
-          )}
-          <div className="certificate-info-box">
-            <div className="certificate-info-box__image"></div>
-            <div className="certificate-info-box__text">
-              <div className="certificate-info-box__text__name">{item?.name}</div>
-              <div className="certificate-info-box__text__date">
-                <div className="certificate-info-box__text__date-receipt">
-                  접수일 | {formatDate(item?.applicationDate)}
-                </div>
-                <div className="certificate-info-box__text__date-test">
-                  시험일 | {formatDate(item?.examDate)}
+      {data?.map((item, index) => {
+        let moveById = -1;
+        if (isCheckBox && isMailingContent(item)) {
+          moveById = item.mailingId;
+        } else if (!isCheckBox && !isMailingContent(item)) {
+          moveById = item.id;
+        } else {
+          return null;
+        }
+
+        return (
+          <div key={index} className="certificate-info">
+            {isCheckBox && isMailingContent(item) && (
+              <div className="certificate-info-checkbox">
+                {checkArr.includes(item.mailingId) ? (
+                  <CheckboxChecked onClick={() => handleCheckArr(item.mailingId)} />
+                ) : (
+                  <CheckboxEmpty onClick={() => handleCheckArr(item.mailingId)} />
+                )}
+              </div>
+            )}
+            <div className="certificate-info-box" onClick={() => moveToCertDetailById(moveById)}>
+              <div className="certificate-info-box__image">
+                <img src={QNetImage} alt="자격증 이미지" />
+              </div>
+              <div className="certificate-info-box__text">
+                <div className="certificate-info-box__text__name">{item?.name}</div>
+                <div className="certificate-info-box__text__date">
+                  <div className="certificate-info-box__text__date-receipt">
+                    접수일 | {formatDate(item?.applicationDate) || '-'}
+                  </div>
+                  <div className="certificate-info-box__text__date-test">
+                    시험일 | {formatDate(item?.examDate) || '-'}
+                  </div>
                 </div>
               </div>
+              {/*추후 캘린더 기능 추가 예정*/}
+              {/*{requiredCalendar && (*/}
+              {/*    <div className="certificate-info-calendar">*/}
+              {/*        <svg xmlns="http://www.w3.org/2000/svg" fill="none"*/}
+              {/*             viewBox="0 0 24 24"*/}
+              {/*             strokeWidth={1.5}*/}
+              {/*             stroke="currentColor"*/}
+              {/*             className="certificate-info-calendar__icon">*/}
+              {/*            <path strokeLinecap="round" strokeLinejoin="round"*/}
+              {/*                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"/>*/}
+              {/*        </svg>*/}
+              {/*        <div className="certificate-info-calendar__count">*/}
+              {/*            {item?.CalendarSubscriptionCount}*/}
+              {/*        </div>*/}
+              {/*    </div>*/}
+              {/*)}*/}
             </div>
-            {/*추후 캘린더 기능 추가 예정*/}
-            {/*{requiredCalendar && (*/}
-            {/*    <div className="certificate-info-calendar">*/}
-            {/*        <svg xmlns="http://www.w3.org/2000/svg" fill="none"*/}
-            {/*             viewBox="0 0 24 24"*/}
-            {/*             strokeWidth={1.5}*/}
-            {/*             stroke="currentColor"*/}
-            {/*             className="certificate-info-calendar__icon">*/}
-            {/*            <path strokeLinecap="round" strokeLinejoin="round"*/}
-            {/*                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"/>*/}
-            {/*        </svg>*/}
-            {/*        <div className="certificate-info-calendar__count">*/}
-            {/*            {item?.CalendarSubscriptionCount}*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*)}*/}
           </div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
